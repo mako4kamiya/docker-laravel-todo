5eb7cf8ac3c054fc04741f1c322c9d56eeba7545
mako4kamiya <mako4kamiya@gmail.com>
2020-07-19 16:10:42 +0900

    タスクの一覧表示の実装

--- application/app/Http/Controllers/TaskController.php
+++ application/app/Http/Controllers/TaskController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index(int $id)
     {
-        $folders = Folder::all();
+        $folders = Folder::all(); // すべてのフォルダを取得する
+        $current_folder = Folder::find($id); // 選ばれたフォルダを取得する
+        $tasks = Task::where('folder_id', $current_folder->id)->get(); // 選ばれたフォルダに紐づくタスクを取得する
+
         return view('tasks/index', [
             'folders' => $folders,
-            'current_folder_id' => $id,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
         ]);
     }
 }
