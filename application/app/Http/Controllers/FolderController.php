3aa6cb4d85e120c7e9855b0a5685f2364300daed
mako4kamiya <mako4kamiya@gmail.com>
2020-07-25 19:41:23 +0900

    フォルダ作成機能の実装

---
+++ application/app/Http/Controllers/FolderController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Folder;
+use Illuminate\Http\Request;
+
+class FolderController extends Controller
+{
+    public function showCreateForm()
+    {
+        return view('folders/create');
+    }
+
+    public function create(Request $request)// 引数にインポートしたRequestクラスを受け入れる
+    {
+        $folder = new Folder();// フォルダモデルのインスタンスを作成する
+        $folder->title = $request->title;// タイトルに入力値を代入する
+        $folder->save();// インスタンスの状態をデータベースに書き込む
+        return redirect()->route('tasks.index', [
+            'id' => $folder->id,
+        ]);
+    }
+}
