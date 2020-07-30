9a61e6b915bda8ff341987688ba465d9e07074eb
mako4kamiya <mako4kamiya@gmail.com>
2020-07-30 15:52:36 +0900

    6_ToDoアプリのタスク作成機能を作る(testはとばした。sqlite)

--- application/app/Http/Controllers/TaskController.php
+++ application/app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateTask;
 use App\Folder;
 use App\Task;
 use Illuminate\Http\Request;
@@ -25,6 +26,18 @@ class TaskController extends Controller
     {
         return view('tasks/create', [
             'folder_id' => $id
+        ]);
+    }
+
+    public function create(int $id, CreateTask $request)
+    {
+        $current_folder = Folder::find($id);
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+        $current_folder->tasks()->save($task);
+        return redirect()->route('tasks.index', [
+            'id' => $current_folder->id,
         ]);
     }
 }
